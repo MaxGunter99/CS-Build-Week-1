ff281d7e78028c1cb18b065c2a7b0c4e4f00702f
maxgunter99 <maxgunter99@gmail.com>
2019-10-21 15:43:14 -0500

    setup complete

--- util/create_world.py
+++ util/create_world.py
@@ -4,8 +4,7 @@ from adventure.models import Player, Room
 
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+r_outside = Room(title="Outside Cave Entrance", description="North of you, the cave mount beckons")
 
 r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
 passages run north and east.""")
@@ -44,4 +43,3 @@ players=Player.objects.all()
 for p in players:
   p.currentRoom=r_outside.id
   p.save()
-
